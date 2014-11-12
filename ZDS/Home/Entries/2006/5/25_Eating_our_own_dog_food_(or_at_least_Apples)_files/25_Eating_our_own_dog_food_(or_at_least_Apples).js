5fbd02c8101ecd236803dd327e59216d671feab3
Sherri Meadows <sherri@dragonforged.com>
2014-11-12 13:24:20 -0700

    Updated to show the MartianCraft acquisition info

--- ZDS/Home/Entries/2006/5/25_Eating_our_own_dog_food_(or_at_least_Apples)_files/25_Eating_our_own_dog_food_(or_at_least_Apples).js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20120522
-
-setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),stroke_1:new IWStrokeParts([{rect:new IWRect(-1,1,2,29),url:'25_Eating_our_own_dog_food_%28or_at_least_Apples%29_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'25_Eating_our_own_dog_food_%28or_at_least_Apples%29_files/stroke_1.png'},{rect:new IWRect(1,-1,750,2),url:'25_Eating_our_own_dog_food_%28or_at_least_Apples%29_files/stroke_2.png'},{rect:new IWRect(751,-1,2,2),url:'25_Eating_our_own_dog_food_%28or_at_least_Apples%29_files/stroke_3.png'},{rect:new IWRect(751,1,2,29),url:'25_Eating_our_own_dog_food_%28or_at_least_Apples%29_files/stroke_4.png'},{rect:new IWRect(751,30,2,2),url:'25_Eating_our_own_dog_food_%28or_at_least_Apples%29_files/stroke_5.png'},{rect:new IWRect(1,30,750,2),url:'25_Eating_our_own_dog_food_%28or_at_least_Apples%29_files/stroke_6.png'},{rect:new IWRect(-1,30,2,2),url:'25_Eating_our_own_dog_food_%28or_at_least_Apples%29_files/stroke_7.png'}],new IWSize(752,31))});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('25_Eating_our_own_dog_food_(or_at_least_Apples)_files/25_Eating_our_own_dog_food_(or_at_least_Apples)Moz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');fixAllIEPNGs('../../../../Media/transparent.gif');fixupAllIEPNGBGs();BlogFixupPreviousNext();applyEffects()}
--- ZDS/Products_files/Products.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20120522
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWStrokeParts([{rect:new IWRect(-1,1,2,29),url:'Products_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Products_files/stroke_1.png'},{rect:new IWRect(1,-1,750,2),url:'Products_files/stroke_2.png'},{rect:new IWRect(751,-1,2,2),url:'Products_files/stroke_3.png'},{rect:new IWRect(751,1,2,29),url:'Products_files/stroke_4.png'},{rect:new IWRect(751,30,2,2),url:'Products_files/stroke_5.png'},{rect:new IWRect(1,30,750,2),url:'Products_files/stroke_6.png'},{rect:new IWRect(-1,30,2,2),url:'Products_files/stroke_7.png'}],new IWSize(752,31)),stroke_2:new IWEmptyStroke(),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Products_files/ProductsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();applyEffects()}
